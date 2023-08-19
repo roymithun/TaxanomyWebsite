eb0bf5df98fbb99251865a8fe0adc3c07fe33805
Cool-Sonic-Guy <142439315+Cool-Sonic-Guy@users.noreply.github.com>
2023-08-19 21:15:05 +0400

    chore: added new plants

--- script_flora.js
+++ script_flora.js
@@ -8,15 +8,30 @@ const jsonDataElement = document.getElementById('flora-json-data');
 const jsonData = [
     {
       "name": "Tulip",
-      "scientific_name": "Tulipa spp",
+      "scientific_name": "Tulipa",
       "url": "https://tinyurl.com/47hnr6jv"
     },
     {
       "name": "Rose",
-      "scientific_name": "Rosa spp",
+      "scientific_name": "Rosa",
       "url": "https://cdn.wallpapersafari.com/69/75/FmR2l5.jpg"
     },
     {
+      "name": "Carnation",
+      "scientific_name": "Dianthus caryophyllus",
+      "url": "https://images.freeimages.com/images/large-previews/0fd/carnation-flower-1633539.jpg"
+    },
+	{
+      "name": "Daffodil",
+      "scientific_name": "Syringa vulgaris",
+      "url": "https://mcdn.wallpapersafari.com/medium/14/33/YFfGz7.jpg"
+    },
+	{
+      "name": "Lilac",
+      "scientific_name": "Dianthus caryophyllus",
+      "url": "https://as1.ftcdn.net/v2/jpg/02/07/98/94/1000_F_207989400_FGXAWjokAh1ZnXmSjbJlvr9rahDqtsEy.jpg"
+    },
+	{
       "name": "Carnation",
       "scientific_name": "Dianthus caryophyllus",
       "url": "https://images.freeimages.com/images/large-previews/0fd/carnation-flower-1633539.jpg"
